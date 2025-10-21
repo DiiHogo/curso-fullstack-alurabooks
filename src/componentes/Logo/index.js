f5bdff0e67b77737b7cd4bea07165ad62b0fbd4d
Diogo Siscati <diogosiscatitrabalho@gmail.com>
2025-10-21 17:24:30 -0300

    criando app e primeiros passos

---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,14 @@
+import './style.css'
+import logo from '../../imgs/logo.svg'
+
+function Logo (){
+    return(
+        <div className='Logo'>
+            <img src={logo} alt='Logo da Alura Books' className='Logo_img'></img>
+            <p><strong>Alura</strong>Books</p>
+        </div>
+    );
+    
+};
+
+export default Logo;
