dac4c82c07f2cef1d294d8cb48da0c56370410ee
Diogo Siscati <diogosiscatitrabalho@gmail.com>
2025-10-23 16:42:36 -0300

    Melhorando o header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-import Logo from './componentes/Logo'; 
+import Logo from './componentes/Logo';
+import Perfil from './imgs/perfil.svg';
+import Sacola from './imgs/sacola.svg';
+import OpcoesHeader from './componentes/OpcoesHeader';
+const icones = [Perfil, Sacola];
 
 function App() {
   return (
     <div className='App'>
       <header className='App_header'>
         <Logo></Logo>
+        <OpcoesHeader></OpcoesHeader>
+
+        <ul className='icones'>
+          {icones.map( (icone) => (
+            <li className='icone'><img src={icone} alt='icone'></img></li>
+          ) )}
+        </ul>
       </header>
     </div>
   );
--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -7,8 +7,8 @@ function Logo (){
             <img src={logo} alt='Logo da Alura Books' className='Logo_img'></img>
             <p><strong>Alura</strong>Books</p>
         </div>
-    );
+    )
     
-};
+}
 
-export default Logo;
+export default Logo
